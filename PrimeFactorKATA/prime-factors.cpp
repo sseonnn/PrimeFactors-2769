3999cbfd142e30b16bb9c011dd3fc6f2fb1acf2c
jeong.lee <com2769@gmail.com>
2025-07-02 10:24:40 +0900

    [refactoring] 67

--- PrimeFactorKATA/prime-factors.cpp
+++ PrimeFactorKATA/prime-factors.cpp
@@ -6,18 +6,11 @@ class PrimeFactors {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number > 1) {
-			int divisior = 2;
-			if (number == 4 || number == 6 || number == 9 || number == 12) {
-				for (divisior = 2; number > 1; divisior++) {
-					while (number % divisior == 0) {
-						result.push_back(divisior);
-						number /= divisior;
-					}
-				}
-			}
-			else {
-				result.push_back(number);
+
+		for (int divisior = 2; number > 1; divisior++) {
+			while (number % divisior == 0) {
+				result.push_back(divisior);
+				number /= divisior;
 			}
 		}
 		return result;
