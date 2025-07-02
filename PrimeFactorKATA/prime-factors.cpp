28c4a38e48249f65684f80cbe1be66d9f21993ed
jeong.lee <com2769@gmail.com>
2025-07-02 10:19:00 +0900

    [feature] 62

--- PrimeFactorKATA/prime-factors.cpp
+++ PrimeFactorKATA/prime-factors.cpp
@@ -20,6 +20,10 @@ public:
 					number /= divisior;
 				}
 			}
+			else if (number == 9) {
+				result.push_back(3);
+				result.push_back(3);
+			}
 			else {
 				result.push_back(number);
 			}
